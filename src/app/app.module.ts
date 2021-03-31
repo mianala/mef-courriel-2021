f494b37ed3ac1c33b347b135fc9c563ee7473994
Loharano <mianalaloharano@gmail.com>
2021-03-31 08:08:35 +0300

    app: help, docs, search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.
 import { AdminComponent } from './admin/admin.component';
 import { LogsComponent } from './admin/logs/logs.component';
 import { FeedbacksComponent } from './admin/feedbacks/feedbacks.component';
+import { DocsComponent } from './docs/docs.component';
+import { SearchComponent } from './search/search.component';
+import { HelpComponent } from './help/help.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +23,10 @@ import { FeedbacksComponent } from './admin/feedbacks/feedbacks.component';
     ForgotPasswordComponent,
     AdminComponent,
     LogsComponent,
-    FeedbacksComponent
+    FeedbacksComponent,
+    DocsComponent,
+    SearchComponent,
+    HelpComponent
   ],
   imports: [
     BrowserModule,
