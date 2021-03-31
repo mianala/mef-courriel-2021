d839d66e91fd09e028e38fd3cce156bbb49d052a
Loharano <mianalaloharano@gmail.com>
2021-03-31 08:33:50 +0300

    app/modules/app-routing.module: base routes

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from '../app.component';
+import { AuthComponent } from '../auth/auth.component';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+import { HelpComponent } from '../help/help.component';
+import { SearchComponent } from '../search/search.component';
+import { ViewerComponent } from '../viewer/viewer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthComponent,
+  },
+  {
+    path: 'search',
+    component: SearchComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'viewer',
+    component: ViewerComponent,
+  },
+  {
+    path: 'help',
+    component: HelpComponent,
+  },
+  {
+    path: 'app',
+    component: AppComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
