8646c3111815afe9c91a01708f6ce2ac305ff1c6
Loharano <mianalaloharano@gmail.com>
2021-05-18 20:14:36 +0300

    created global.classes

--- src/app/admin/users/inactive-users/inactive-users.component.ts
+++ src/app/admin/users/inactive-users/inactive-users.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { combineLatest } from 'rxjs';
+import { startWith, map } from 'rxjs/operators';
+import { User } from 'src/app/classes/user';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -7,9 +11,24 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./inactive-users.component.scss'],
 })
 export class InactiveUsersComponent implements OnInit {
-  inactiveUsers$ = this.userService.inactivatedUsers$;
+  filter = new FormControl('');
+  filteredUsers$ = combineLatest([
+    this.userService.inactivatedUsers$,
+    this.filter.valueChanges.pipe(startWith('')),
+  ]).pipe(
+    map(([users, query]) => {
+      const q = new RegExp(query, 'i');
 
-  constructor(private userService: UserService) {}
+      return users.filter((user: User) => {
+        return (
+          user.entity.short.match(q) ||
+          user.firstname.match(q) ||
+          user.lastname.match(q)
+        );
+      });
+    })
+  );
+  constructor(public userService: UserService) {}
 
   ngOnInit(): void {}
 }
