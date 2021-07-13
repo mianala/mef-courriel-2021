f9e121d25dba2d6ece5043093cfc9273e67a01cb
Loharano <mianalaloharano@gmail.com>
2021-07-13 07:36:15 +0300

    app/modules/app-routing.module: started using hash #

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -249,7 +249,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
